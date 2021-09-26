c82d92ed85434bd7741e19c0d8107ae62f9775e9
kuza-I <ka0310uz71ma0@gmail.com>
2021-09-26 18:16:59 +0900

    JavaScript関係のところとりあえず遊んでみた

--- 01_テキスト/01_JavaScriptのオブジェクト/main.js
+++ 01_テキスト/01_JavaScriptのオブジェクト/main.js
@@ -5,12 +5,35 @@ const hikakin = {
   isHappy: true,
   address: {
     country: "日本",
-    city: "東京"
+    city: "東京",
   },
   sayHello: function() {
     console.log("ブンブンハロー YouTube")
-  }
+  },
 }
+
+const kuzuha = {
+  name: "葛葉",
+  age: 100,
+  like: ["ゲーム", "金", "甘いもの"],
+  dislike: ["炎", "炎上", "苦いもの"],
+
+  isHappy: true,
+  profile: {
+    height: 178,
+    weight: 62,
+    shoes: 27,
+  },
+  saySuperChat: function() {
+    console.log("スーパーチャット、ありがとうございまーす！")
+  },
+}
+
+console.log(hikakin.address)
+
+console.log(kuzuha.like)
 
 // 練習問題
 // hikakin オブジェクトに入っている sayHello メソッドを実行してください。
+hikakin.sayHello()
+kuzuha.saySuperChat()
