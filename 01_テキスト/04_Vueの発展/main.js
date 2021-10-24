a049ce1fd0c2170c44ffe4c75bd1d97aa245a47d
kuza-I <ka0310uz71ma0@gmail.com>
2021-10-24 20:55:44 +0900

    Vueのちょいアレンジ

--- 01_テキスト/04_Vueの発展/main.js
+++ 01_テキスト/04_Vueの発展/main.js
@@ -2,31 +2,32 @@
 // Vue はページの一部分（ el で指定した範囲）に適用できるので、1ページにいっぱいあっても大丈夫です
 
 new Vue({
-  el: '#v-if',
+  el: "#v-if",
   data: {
-    seen: false
+    seen: false,
   },
   methods: {
-    hyouji: function () {
+    hyouji: function() {
       this.seen = true
-    }
-  }
+    },
+  },
 })
 
 new Vue({
   el: "#v-if-sign-in",
   data: {
-    user: null
+    user: null,
   },
   methods: {
     signIn() {
       this.user = {
-        name: "ギーク太郎"
+        name: "ギーク太郎",
+        age: 22,
       }
     },
     signOut() {
       this.user = null
-    }
+    },
   },
 })
 
@@ -34,7 +35,7 @@ new Vue({
   el: "#v-for",
   data: {
     animals: ["ふらみんご", "ごりら", "らいおん"],
-  }
+  },
 })
 
 new Vue({
@@ -43,7 +44,7 @@ new Vue({
     animals: ["ふらみんご", "ごりら", "らいおん"],
   },
   methods: {
-    addAnimal: function () {
+    addAnimal: function() {
       this.animals.push("んらいおん")
     },
   },
@@ -78,7 +79,7 @@ new Vue({
     ],
   },
   computed: {
-    totalPrice: function () {
+    totalPrice: function() {
       let price = 0
       for (let i = 0; i < this.items.length; i++) {
         price += this.items[i].price * this.items[i].amount
@@ -110,7 +111,7 @@ new Vue({
     ],
   },
   computed: {
-    totalPrice: function () {
+    totalPrice: function() {
       let price = 0
       for (let i = 0; i < this.items.length; i++) {
         price += this.items[i].price * this.items[i].amount
@@ -119,7 +120,7 @@ new Vue({
     },
   },
   methods: {
-    addItem: function (item) {
+    addItem: function(item) {
       item.amount += 1
     },
   },
@@ -138,14 +139,14 @@ new Vue({
     inputValue: "",
     memo: "",
   },
-  created: function () {
+  created: function() {
     const storedMemo = localStorage.memo
     if (storedMemo) {
       this.memo = storedMemo
     }
   },
   methods: {
-    save: function () {
+    save: function() {
       localStorage.memo = this.inputValue
     },
   },
